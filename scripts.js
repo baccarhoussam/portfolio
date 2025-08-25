740eed61634d0a93a2c72ddc672a75f45342a358
baccarhoussam <baccar.houssam@gmail.com>
2025-08-25 11:45:42 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,22 @@
+jQuery.noConflict();
+
+(function($) {
+	$(document).ready(function() {
+
+
+    
+        $('.up').click(function(){
+     
+        $('html, body').animate({scrollTop :0},800);
+              return false;
+        });
+    
+	
+	
+	
+
+	
+	});
+	
+	
+}(jQuery));
